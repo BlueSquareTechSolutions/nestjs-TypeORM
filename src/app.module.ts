38921e890a2c31670987d37d7a7ff5a6d53006af
Vamshree Siramdasu <vamshree@outlook.com>
2021-10-09 20:58:41 +1100

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ArticleModule } from './article/article.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { ProfileModule } from './profile/profile.module';
+import { TagModule } from './tag/tag.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": "localhost",
+      "port": 5999,
+      "username": "postgres",
+      "password": "mysecretpassword",
+      "database": "nestjsrealworld",
+      "entities": ["src/**/**.entity{.ts,.js}"],
+      "synchronize": true
+    }),
+    ArticleModule,
+    UserModule,
+    ProfileModule,
+    TagModule
+  ],
+  controllers: [
+    AppController
+  ],
+  providers: []
+})
+export class ApplicationModule {
+  constructor(private readonly connection: Connection) {}
+}
